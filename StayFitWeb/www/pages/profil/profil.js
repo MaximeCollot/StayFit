0020964c263970ff939d9ae1364ba91200ee55b4
MaximeCollot <maxime.collot.69@gmail.com>
2016-11-27 23:12:09 +0100

    [FRONT] Fix bouton side menu + debut bouton modif profil

--- StayFitWeb/www/pages/profil/profil.js
+++ StayFitWeb/www/pages/profil/profil.js
@@ -1,8 +1,22 @@
 angular.module('profil.controllers', [])
 
 .controller('ProfilCtrl', function($scope) {
+	console.log('ProfilCtrl');
+	$scope.modif = {firstName:'', lastName:'', mail:''};
 
-
+	$scope.doProfilModification = function (modif) {
+		console.log(modif);
+		if (modif.firstName != ''){
+			$scope.user.firstName = modif.firstName;
+		}
+		if (modif.lastName != ''){
+			$scope.user.lastName = modif.lastName;
+		}
+		if (modif.mail != ''){
+			$scope.user.mail = modif.mail;
+		}
+		$scope.modif = {firstName:'', lastName:'', mail:''};
+	}
 
 
 });
